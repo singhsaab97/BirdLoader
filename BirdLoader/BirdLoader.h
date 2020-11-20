4b965f014946671b47790ecbb92ecef19b7f6525
Abhijit Singh <abhijit@noonacademy.com>
2020-11-20 09:27:30 +0530

    Initial project setup

---
+++ BirdLoader/BirdLoader.h
@@ -0,0 +1,18 @@
+//
+//  BirdLoader.h
+//  BirdLoader
+//
+//  Created by Abhijit Singh on 20/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BirdLoader.
+FOUNDATION_EXPORT double BirdLoaderVersionNumber;
+
+//! Project version string for BirdLoader.
+FOUNDATION_EXPORT const unsigned char BirdLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BirdLoader/PublicHeader.h>
+
+
